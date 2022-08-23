781f306ade97655de92ec48b6d16bf7d003ae851
«Rawed» <pepe1@mail.ru>
2022-08-23 18:16:57 +0300

    finish

---
+++ Z2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 100)
+{
+    string str = num.ToString();
+    Console.WriteLine(str[2]);
+}
+else
+{
+    Console.WriteLine("Число меньше трехзначного");
+}
---
+++ Z3/Program.cs
@@ -0,0 +1,67 @@
+﻿
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли 
+// этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+
+if(numberDay < 1 || numberDay > 7)
+{
+    Console.WriteLine("Ошибка");
+    Environment.Exit(0);
+}
+if (numberDay < 6)
+{
+    Console.WriteLine("Рабочий день");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
+
+// Второй вариант c подписанными днями недели:
+// Console.WriteLine("Введите число");
+// int numberDay = Convert.ToInt32(Console.ReadLine());
+
+// if(numberDay < 1 || numberDay > 7)
+// {
+//     Console.WriteLine("Ошибка");
+// }
+// if(numberDay == 1)
+// {
+//     Console.WriteLine("Понедельник - рабочий день");
+// }
+
+// if(numberDay == 2)
+// {
+//     Console.WriteLine("Вторник - рабочий день");
+// }
+
+// if(numberDay == 3)
+// {
+//     Console.WriteLine("Среда - рабочий день");
+// }
+
+// if(numberDay == 4)
+// {
+//     Console.WriteLine("Четверг - рабочий день");
+// }
+
+// if(numberDay == 5)
+// {
+//     Console.WriteLine("Пятница - короткий день");
+// }
+
+// if(numberDay == 6)
+// {
+//     Console.WriteLine("Суббота - выходной");
+// }
+
+// if(numberDay == 7)
+// {
+//     Console.WriteLine("Воскресенье - выходной");
+// }
